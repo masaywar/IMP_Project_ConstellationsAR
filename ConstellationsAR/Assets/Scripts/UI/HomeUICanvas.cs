532b574e9f2231ebea24999da4360f284ff5b759
HIHYUN <khjj2121@ajou.ac.kr>
2021-04-09 10:17:54 +0900

    Combine constellation name button to information penel

--- ConstellationsAR/Assets/Scripts/UI/HomeUICanvas.cs
+++ ConstellationsAR/Assets/Scripts/UI/HomeUICanvas.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using TMPro;
 
 public class HomeUICanvas : UIWindow
 {   
     public GameObject InformationCanvas;
     public GameObject ScreenShotCanvas;
 
-    public Button bttn4OpenInfo;
     public Button bttn4OpenScreenshot;
+    public List<Button> bttn4OpenInfo = new List<Button>();
+    public TextMeshProUGUI ConstellationName;
 
     void Awake() 
-    {   
-        bttn4OpenInfo.onClick.AddListener(OpenInformationCanvas);
+    {           
+
+        foreach (Button button in bttn4OpenInfo)
+        {
+            button.onClick.AddListener(() => OpenInformationCanvas(button.name));
+        }
+        
         bttn4OpenScreenshot.onClick.AddListener(OpenScreenShotCanvas);
     }
 
-    private void OpenInformationCanvas()
+    private void OpenInformationCanvas(string buttonname)
     {
         InformationCanvas.SetActive(true);
+        ConstellationName.text = buttonname;
+
     }
     private void OpenScreenShotCanvas()
     {
